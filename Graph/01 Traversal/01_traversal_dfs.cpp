41fef3faed97ffcea443d1f04b2d05f46fec9f48
Saurabh Kumar Singh <saurabhs051@gmail.com>
2025-04-04 01:58:45 +0530

    Update 01_traversal_dfs.cpp

--- Graph/01 Traversal/01_traversal_dfs.cpp
+++ Graph/01 Traversal/01_traversal_dfs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 	/* 
     *	Traversal of Directed/Undirected graph : DFS
-    *	1. Just don't visit already visited node. (Handling of Cycle and Undirected 2-way edges)
-	*		1.1 Prevents endless loop in case of cycle (directed/undirected graph)
- 	*		1.2	Prevents back traversal of edges in undirected graph
-  	*		1.3 Prevents revisiting already visited node/path in case of Forked directed graph (multiple src pointing to same destination)
-    *	2. Traverse from each source node (Handling of disjoint graph (directed/undirected) and Forked directed graph)
-	*		2.1	Handles traversing all disconnected components in disjoint graphs
- 	*		2.2	Handles visiting all sources in Forked directed graphs
+    *	1. Traverse from each source node (Handling of disjoint graph (directed/undirected) and Forked directed graph)
+	*		1.1	Handles traversing all disconnected components in disjoint graphs
+ 	*		1.2	Handles visiting all sources in Forked directed graphs
+    *	2. Just don't visit already visited node. (Handling of Cycle and Undirected 2-way edges)
+	*		2.1 	Prevents endless loop in case of cycle (directed/undirected graph)
+ 	*		2.2 	Prevents back traversal of edges in undirected graph
+  	*		2.3 	Prevents revisiting already visited node/path in case of Forked directed graph (multiple src pointing to same destination)
     */
     void dfs(vector<vector<int>>& adj, int src, vector<bool>& visited, vector<int>& result)
     {
