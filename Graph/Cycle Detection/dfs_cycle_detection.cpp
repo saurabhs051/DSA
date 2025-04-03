9e8516d9c91fd6565ce0113eaefc1694a5238482
Saurabh Kumar Singh <saurabhs051@gmail.com>
2025-04-03 23:53:13 +0530

    Update dfs_cycle_detection.cpp

--- Graph/Cycle Detection/dfs_cycle_detection.cpp
+++ Graph/Cycle Detection/dfs_cycle_detection.cpp
@@ -1,4 +1,9 @@
 class Solution {
+    /* 
+    *	1. Node should not be in current path (i.e. in current Recursion Stack)
+    *	2. Try from each source node, to tackle case of disjoint graph
+	*	3. No need to visit already visited node (hence, maintain a visited vector, separate from recusion stack)
+    */
     int getCycle(vector<vector<int>>& adj, int src, vector<bool> &visited, vector<bool> &recStack)
     {
         if (recStack[src]) // Answer check first
@@ -15,7 +20,7 @@ class Solution {
             if (getCycle(adj, neighbour, visited, recStack))
                 return true;
         }
-        recStack[src] = false;
+        recStack[src] = false;  // Clear from recursion stack
         return false;
     }
 
@@ -25,10 +30,11 @@ class Solution {
         int A = adj.size();
         vector<bool> visited(A, false);
         vector<bool> recStack(A, false);
+		// Check from each source node
         for (int i = 0; i < A; ++i)
         {
             if (getCycle(adj, i, visited, recStack))
-                return true;
+                return true;	// Early return
         }
         return false;
     }
