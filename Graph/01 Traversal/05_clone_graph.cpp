1a0923edc459507c0ec1b89372277e6570047cc7
Saurabh Kumar Singh <saurabhs051@gmail.com>
2025-04-06 01:08:32 +0530

    Create 05_clone_graph.cpp

---
+++ Graph/01 Traversal/05_clone_graph.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    unordered_map<Node*, Node*> clone;
+public:
+    Node* cloneGraph(Node* node) {
+        // If null node passed
+        if (!node)
+            return nullptr;
+        // Already cloned Node - Return to prevent cycle
+        if (clone[node] != nullptr)
+            return clone[node];
+        
+        // Clone doesn't exist - Create one
+        Node* newNode = new Node(node->val);
+        clone[node] = newNode;
+		// Clone neighbours
+        for (auto& neighbour : node->neighbors)
+            newNode->neighbors.push_back(cloneGraph(neighbour));
+        return newNode;
+    }
+};
