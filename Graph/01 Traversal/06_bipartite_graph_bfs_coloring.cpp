af71f0b3cf4264e8f06018f59e20f760c0700bfc
Saurabh Kumar Singh <saurabhs051@gmail.com>
2025-04-06 17:00:39 +0530

    Create 06_bipartite_graph_bfs_coloring.cpp

---
+++ Graph/01 Traversal/06_bipartite_graph_bfs_coloring.cpp
@@ -0,0 +1,50 @@
+class Solution {
+/* 
+ * Bipartite Graph : Adjacent nodes have different colors (belong to different set)
+ * 1. Traversal : Can do with BFS / DFS
+ * 2. Graph can be disjoint : Start from each unvisited source node
+ * 3. Details :
+ *      3.1 Color during adding to queue(i.e. while visiting)
+*/
+    bool bfs_coloring(vector<vector<int>>& graph, int src, vector<int>& visited)
+    {
+        queue<int> q;
+        q.push(src);
+        visited[src] = 0; // Start with '0' colour
+        while (q.size())
+        {
+            int curNode = q.front(); q.pop();
+            for (auto& neighbour : graph[curNode])
+            {
+                if (visited[neighbour] == -1)   // unvisited
+                {
+                    visited[neighbour] = !visited[curNode]; // apply opposite colour
+                    q.push(neighbour);
+                }
+                else   // already visited / colored node
+                {
+                    if (visited[neighbour] == visited[curNode]) // Adjacent nodes have same color
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> visited(V, -1); // Used for visited tracking as well as colouring of nodes
+
+        // Disconnected Graph : Start from each yet unvisited source node
+        for (int src = 0; src < V; ++src)
+        {
+            if (visited[src] == -1)
+            {
+                if (!bfs_coloring(graph, src, visited))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
