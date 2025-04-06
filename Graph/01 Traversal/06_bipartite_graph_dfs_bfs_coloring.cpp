45aeb9aacbddb6f94c76fb947e25a06df9d1cd1c
Saurabh Kumar Singh <saurabhs051@gmail.com>
2025-04-06 17:18:36 +0530

    Update and rename 06_bipartite_graph_bfs_coloring.cpp to 06_bipartite_graph_dfs_bfs_coloring.cpp

--- Graph/01 Traversal/06_bipartite_graph_dfs_bfs_coloring.cpp
+++ Graph/01 Traversal/06_bipartite_graph_dfs_bfs_coloring.cpp
@@ -1,11 +1,13 @@
-class Solution {
+
 /* 
  * Bipartite Graph : Adjacent nodes have different colors (belong to different set)
  * 1. Traversal : Can do with BFS / DFS
  * 2. Graph can be disjoint : Start from each unvisited source node
  * 3. Details :
- *      3.1 Color during adding to queue(i.e. while visiting)
-*/
+ *      3.1 BFS : Color during adding to queue(i.e. while visiting)
+ * 4. Time complexity = O(V + E) - traversal
+ */
+class Solution {
     bool bfs_coloring(vector<vector<int>>& graph, int src, vector<int>& visited)
     {
         queue<int> q;
@@ -30,7 +32,26 @@ class Solution {
         }
         return true;
     }
-    
+
+    bool dfs_coloring(vector<vector<int>>& graph, int src, vector<int>& visited, int color)
+    {
+        if (visited[src] == -1) // unvisited
+        {
+            visited[src] = color;                  // Color it with given color
+            for (auto& neighbour : graph[src])     // Color neighbours
+            {
+                if (!dfs_coloring(graph, neighbour, visited, !color))  // Early return if not possoble
+                    return false;
+            }
+        }
+        else    // Already visited / coloured
+        {
+            if (visited[src] != color)  // Color different from expected, not bipartite
+                return false;
+        }
+        return true;
+    }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
@@ -41,7 +62,8 @@ public:
         {
             if (visited[src] == -1)
             {
-                if (!bfs_coloring(graph, src, visited))
+                //if (!bfs_coloring(graph, src, visited))
+                if (!dfs_coloring(graph, src, visited, 0))
                     return false;
             }
         }
