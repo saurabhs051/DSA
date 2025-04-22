db305846f0fbaf09df7ccd1e47be539cba0a09d0
Saurabh Kumar Singh <saurabhs051@gmail.com>
2025-04-23 02:57:38 +0530

    Update 01_xor_find_2_unique_numbers.cpp

--- 01 Data Structure and Algorithms/Bit Manipulation/01_xor_find_2_unique_numbers.cpp
+++ 01 Data Structure and Algorithms/Bit Manipulation/01_xor_find_2_unique_numbers.cpp
@@ -3,19 +3,19 @@
 
 class Solution {
 public:
-    /**
-     * Finds the two unique numbers in an array where all other numbers (except these 2 unique numbers) appears twice.
-	 * Theoretical concept : XOR of different bits = 1, XOR of same bits = 0
-	 * Practical Concept 1 : num ^ 0 = num (i.e. initialize the variable that will be used for XORS with 0, it won't affect the result)
-  	 * Practical Concept 2 : num ^ num = 0 (i.e. if same number occurring even times, it will cancel out) - VERY IMPORTANT
-	 * Practical Concept 3 : To check whether a bit is set, check : if (num & (1 << position))
-     * Practical Concept 4 : To set a bit at a position, do : num |= (1 << position)
-	 * Practical Concept 5 : To unset a bit at a position, do : num = num & ~(1 << position)
-     * Practical Concept 6 : Always use parenthesis while using bitwise operators &, |, ^ in conditions, because they have less precedence than == , e.g. if (0 == (b & c))
-	 * Practical Concept 7 : Remember, (num & (1 << position)) is not equal to 1, it's equal to a number like 00100 (i.e. 4 in this case), so checking if(num & (1 << position))  is right, but if((num & (1 << position)) == 1) is wrong.
-     * Practical Concept 8 : (n & n-1) unsets last set bit of n. 
-	 * This last concept is used to check number of set bits in a number. e.g. while (n) { n = n & (n - 1); count++; }
-     * It is also used to check if a number is power of 2 (i.e. it has only 1 set bit). e.g. if (n && (n & (n - 1)) == 0) // if n is not equal to 0 and n is power of 2
+    	/**
+     	* Finds the two unique numbers in an array where all other numbers (except these 2 unique numbers) appears twice.
+	* Theoretical concept : XOR of different bits = 1, XOR of same bits = 0
+ 	* Practical Concept 1 : num ^ 0 = num (i.e. initialize the variable that will be used for XORS with 0, it won't affect the result)
+ 	* Practical Concept 2 : num ^ num = 0 (i.e. if same number occurring even times, it will cancel out) - VERY IMPORTANT
+ 	* Practical Concept 3 : To check whether a bit is set, check : if (num & (1 << position))
+     	* Practical Concept 4 : To set a bit at a position, do : num |= (1 << position)
+ 	* Practical Concept 5 : To unset a bit at a position, do : num &= ~(1 << position)
+     	* Practical Concept 6 : Always use parenthesis while using bitwise operators &, |, ^ in conditions, because they have less precedence than == , e.g. if (0 == (b & c))
+ 	* Practical Concept 7 : Remember, (num & (1 << position)) is not equal to 1, it's equal to a number like 00100 (i.e. 4 in this case), so checking if(num & (1 << position))  is right, but if((num & (1 << position)) == 1) is wrong.
+     	* Practical Concept 8 : (n & n-1) unsets last set bit of n. 
+ 	* This last concept is used to check number of set bits in a number. e.g. while (n) { n = n & (n - 1); count++; }
+     	* It is also used to check if a number is power of 2 (i.e. it has only 1 set bit). e.g. if (n && (n & (n - 1)) == 0) // if n is not equal to 0 and n is power of 2
      *
      * @param nums Input vector of integers
      * @return A vector containing the two unique numbers, sorted in ascending order
